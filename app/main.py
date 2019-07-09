88fe26734ea809853fd5d6aa2937d3f1e7bdfcbd
pkosa <pkosa@xmatters.com>
2019-07-08 17:16:54 -0700

    adding food seeking

--- app/main.py
+++ app/main.py
@@ -33,15 +33,34 @@ def start():
 @bottle.post('/move')
 def move():
     data = bottle.request.json
-
-    # TODO: Do things with data
-    directions = ['up', 'down', 'left', 'right']
-
     return {
-        'move': random.choice(directions),
+        'move': findFood(data),
         'taunt': 'Come get some!'
     }
 
+def getSnake(gameState, id):
+   for snake in gameState["snakes"]:
+       if snake["id"] == id :
+        return snake
+def findFood(gameState):
+
+  mySnake = getSnake(gameState, gameState["you"])
+  head = mySnake["coords"][0]
+  if gameState["food"][0][0] < head[0]: 
+        move = "left"
+
+
+  if gameState["food"][0][0] > head[0]:
+        move = "right"
+
+
+  if gameState["food"][0][1] < head[1]:
+        move = "up"
+
+
+  if gameState["food"][0][1] > head[1]:
+        move = "down"
+  return move 
 
 # Expose WSGI app (so gunicorn can find it)
 application = bottle.default_app()
